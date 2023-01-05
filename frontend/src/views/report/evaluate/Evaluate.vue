6e490316acc7e8b1f2ce3fb99cf4c684b311e169
悲伤的橘子树 <1258846421@qq.com>
2023-01-05 23:11:34 +0800

    fix -员工评价统计

--- frontend/src/views/report/evaluate/Evaluate.vue
+++ frontend/src/views/report/evaluate/Evaluate.vue
@@ -5,21 +5,27 @@
         <div style="background:#ECECEC; padding:30px;margin-top: 30px">
           <a-row :gutter="25">
             <a-col :span="8" v-for="(item, index) in evaluateList" :key="index">
-              <a-card :bordered="false">
+              <a-card :bordered="false" hoverable @click="checkEvaluate(item)">
                 <span slot="title">
                   <span style="font-size: 14px;font-family: SimHei">
                     {{ item.staff.name }}
                     <span>【{{ item.staff.code }}】</span>
                   </span>
                 </span>
-                <p>card content</p>
+                <div style="font-size: 14px;font-family: SimHei;padding: 0px 20px">
+                  <a-row :gutter="15">
+                    <a-col :span="12" style="margin-bottom: 5px">准时得分: {{item.scheduleScore}}</a-col>
+                    <a-col :span="12" style="margin-bottom: 5px">维修得分: {{item.repairScore}}</a-col>
+                    <a-col :span="12" style="margin-bottom: 5px">服务得分: {{item.serviceScore}}</a-col>
+                  </a-row>
+                </div>
               </a-card>
             </a-col>
           </a-row>
         </div>
       </a-col>
       <a-col :span="7">
-        <a-col :span="24">
+        <a-col :span="24" style="margin-top: 30px">
           <apexchart type="radar" height="350" :options="chartOptions" :series="series"></apexchart>
         </a-col>
       </a-col>
@@ -35,7 +41,7 @@ export default {
       evaluateList: [],
       series: [{
         name: '得分',
-        data: [80, 50, 30, 40]
+        data: []
       }],
       chartOptions: {
         chart: {
@@ -55,6 +61,11 @@ export default {
     this.getEvaluateList()
   },
   methods: {
+    checkEvaluate (score) {
+      let data = [score.scheduleScore, score.repairScore, score.serviceScore, score.score, 100]
+      this.series[0].data = data
+      this.chartOptions.title.text = score.staff.name + '评价得分'
+    },
     getEvaluateList () {
       this.$get(`/cos/staff-evaluation/staff/evaluate`).then((r) => {
         this.evaluateList = r.data.data
